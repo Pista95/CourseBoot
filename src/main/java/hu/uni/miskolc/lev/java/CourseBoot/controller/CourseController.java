cd47f1ee046a589c5675eb04921a0821b64085a5
Pista95 <berzi@citromail.hu>
2022-05-13 15:42:38 +0200

    4. Commit

--- src/main/java/hu/uni/miskolc/lev/java/CourseBoot/controller/CourseController.java
+++ src/main/java/hu/uni/miskolc/lev/java/CourseBoot/controller/CourseController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.List;
 @Controller
 public class CourseController {
-    private CourseService courseService;
+    private final CourseService courseService;
     public CourseController(CourseService courseService) {
         this.courseService = courseService;
     }
