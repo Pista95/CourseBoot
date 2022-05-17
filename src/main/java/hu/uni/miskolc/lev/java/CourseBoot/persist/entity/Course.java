cadcdd90733187b72bf8611ba295981feb102c69
Pista95 <berzi@citromail.hu>
2022-05-17 23:55:22 +0200

    Mentes

--- src/main/java/hu/uni/miskolc/lev/java/CourseBoot/persist/entity/Course.java
+++ src/main/java/hu/uni/miskolc/lev/java/CourseBoot/persist/entity/Course.java
@@ -1,15 +1,12 @@
 package hu.uni.miskolc.lev.java.CourseBoot.persist.entity;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
 import java.util.Set;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -20,10 +17,8 @@ public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int course_id;
-
     //@NotBlank
     private String name="info";
-
     @OneToMany(mappedBy = "course")
      Set<CourseRegistration> courseregistration;
 }
