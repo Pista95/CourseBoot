4dba1418e2131d45daf066618dbfc726756adcc1
Pista95 <berzi@citromail.hu>
2022-05-18 22:54:11 +0200

    Mentes

---
+++ src/main/java/hu/uni/miskolc/lev/java/CourseBoot/controller/GetAllController.java
@@ -0,0 +1,16 @@
+package hu.uni.miskolc.lev.java.CourseBoot.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class GetAllController {
+    @GetMapping(value = "/getall", produces = MediaType.TEXT_HTML_VALUE)
+    @ResponseBody
+    public String welcomeAsHTML() {
+        return "<html>\n" + "<header><title>Welcome</title></header>\n" +
+                "<body>\n" + "Hello world\n" + "</body>\n" + "</html>";
+    }
+}
