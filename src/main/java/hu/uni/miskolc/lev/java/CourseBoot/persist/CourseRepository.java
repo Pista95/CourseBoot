9db6064061157f349931e862d371c51f029476e4
Pista95 <berzi@citromail.hu>
2022-05-20 01:33:27 +0200

    Program ok

--- src/main/java/hu/uni/miskolc/lev/java/CourseBoot/persist/CourseRepository.java
+++ src/main/java/hu/uni/miskolc/lev/java/CourseBoot/persist/CourseRepository.java
@@ -1,8 +1,12 @@
 package hu.uni.miskolc.lev.java.CourseBoot.persist;
 
 import hu.uni.miskolc.lev.java.CourseBoot.persist.entity.Course;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface CourseRepository extends CrudRepository<Course, Integer> { }
+public interface CourseRepository extends CrudRepository<Course, Integer> {
+    @Query("select c from Course c where c.course_id = ?1")
+    CourseRepository findAllByCourse_id(int id);
+}
